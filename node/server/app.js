f950be02c30ae9b5d43d2032fa40a5ec0c40489f
jeongjulee42 <ljj3342@naver.com>
2022-11-03 12:33:24 +0900

    Feat: server

--- node/server/app.js
+++ node/server/app.js
@@ -1,11 +1,35 @@
 const http = require("http");
+const fs = require("fs");
+const ejs = require("ejs");
+
+const name = "jeongju";
+const courses = [
+  { name: "HTML" },
+  { name: "CSS" },
+  { name: "JS" },
+  { name: "Node" },
+  { name: "FrontEnd" },
+];
 
 const server = http.createServer((req, res) => {
-  console.log("server Start");
-  console.log(req.headers);
-  console.log(req.httpVersion);
-  console.log(req.url);
-  res.write("Hi bro!!!");
-  res.end();
+  const url = req.url;
+  const method = req.method;
+  if (method === "GET") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(courses));
+  } else if (method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const bodyStr = Buffer.concat(body).toString();
+      const course = JSON.parse(bodyStr);
+      courses.push(course);
+      console.log(course);
+      res.writeHead(201);
+      res.end();
+    });
+  }
 });
-server.listen(8080);
+server.listen(3000);
