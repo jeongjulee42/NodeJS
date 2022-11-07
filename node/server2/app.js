c622272887f8c508e1c379ad32b736f8fb41c91e
jeongjulee42 <ljj3342@naver.com>
2022-11-07 12:09:25 +0900

    Feat: res

--- node/server2/app.js
+++ node/server2/app.js
@@ -1,10 +1,10 @@
 import express from "express";
 
 const app = express();
-app.get("/sky/:idd", (req, res, next) => {
+app.get("/", (req, res, next) => {
   console.log(req.params);
   console.log(req.query);
-  res.send(req.query.key);
+  res.status(200).send("ok");
 });
 
 app.listen(8080);
