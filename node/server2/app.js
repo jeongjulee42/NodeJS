a2a83c0cc55a5fa9c2e9ed71ac4708ae3bda17b9
jeongjulee42 <ljj3342@naver.com>
2022-11-08 10:49:37 +0900

    Feat: add middleware

--- node/server2/app.js
+++ node/server2/app.js
@@ -1,10 +1,19 @@
 import express from "express";
 
 const app = express();
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("first");
+    next("route");
+  },
+  (req, res, next) => {
+    console.log("first-2");
+    next();
+  }
+);
 app.get("/", (req, res, next) => {
-  console.log(req.params);
-  console.log(req.query);
-  res.status(200).send("ok");
+  console.log("second");
 });
 
 app.listen(8080);
