56670cf7c0b571a62325e8dead94950dc76b7194
jeongjulee42 <ljj3342@naver.com>
2022-11-09 12:27:46 +0900

    Feat: add cookieparser

--- node/11-express/cors/app.js
+++ node/11-express/cors/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://127.0.0.1:5500"],
@@ -11,6 +13,7 @@ app.use(
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
+  console.log(req.cookies);
 });
 
 app.listen(8080);
