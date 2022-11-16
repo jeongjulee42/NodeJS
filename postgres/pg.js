350a242ace6291ed2de63a1fe430865d24c9450d
jeongjulee42 <ljj3342@naver.com>
2022-11-16 14:15:03 +0900

    Feat: add DB

--- postgres/pg.js
+++ postgres/pg.js
@@ -1,1 +1,10 @@
-const knex = require("./knex/knex.js");
+const knex = require('./knex/knex.js');
+
+// Create Table
+
+knex.schema.createTable('book', function (table){
+    table.increments();
+    table.string('name');
+}).finally(function () {
+    console.log("Done");
+})
