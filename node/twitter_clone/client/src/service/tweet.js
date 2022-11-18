8ded75ac621c0729afb4cee2ba986e6233aa216e
jeongjulee42 <ljj3342@naver.com>
2022-11-18 12:39:28 +0900

    Feat: add tweets in src

--- node/twitter_clone/client/src/service/tweet.js
+++ node/twitter_clone/client/src/service/tweet.js
@@ -1,55 +1,44 @@
 export default class TweetService {
-  constructor(baseURL) {
-    this.baseURL = "http://localhost:8080";
+  constructor(http, tokenStorage) {
+    this.http = http;
+    this.tokenStorage = tokenStorage;
   }
 
   async getTweets(username) {
-    let query = username ? `?username=${username}` : "";
-    const response = await fetch(`${this.baseURL}/tweets${query}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
+    const query = username ? `?username=${username}` : '';
+    return this.http.fetch(`/tweets${query}`, {
+      method: 'GET',
+      headers: this.getHeaders(),
     });
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/tweets/`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text, username: "elloe", name: "Elloe" }),
+    return this.http.fetch(`/tweets`, {
+      method: 'POST',
+      headers: this.getHeaders(),
+      body: JSON.stringify({ text, username: 'ellie', name: 'Ellie' }),
     });
-    const data = await response.json();
-    if (response.status !== 201) {
-      throw new Error(data.message);
-    }
-    return data;
   }
 
   async deleteTweet(tweetId) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
+    return this.http.fetch(`/tweets/${tweetId}`, {
+      method: 'DELETE',
+      headers: this.getHeaders(),
     });
-    if (response.status !== 204) {
-      const data = await response.json();
-      throw new Error();
-    }
   }
 
   async updateTweet(tweetId, text) {
-    const response = await fetch(`${this.baseURL}/tweets/${tweetId}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
+    return this.http.fetch(`/tweets/${tweetId}`, {
+      method: 'PUT',
+      headers: this.getHeaders(),
       body: JSON.stringify({ text }),
     });
-    const data = await response.json();
-    if (response.status !== 200) {
-      throw new Error(data.message);
-    }
-    return data;
+  }
+
+  getHeaders() {
+    const token = this.tokenStorage.getToken();
+    return {
+      Authorization: `Bearer ${token}`,
+    };
   }
 }
