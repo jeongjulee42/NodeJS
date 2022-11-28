19491ab1ac81bbb3b74d0995349a08959c077c4c
jeongjulee42 <ljj3342@naver.com>
2022-11-28 18:24:20 +0900

    Feat: add writeStream

--- node_exer/app.js
+++ node_exer/app.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
-const buf = Buffer.from('Hi');
 
-const buf2 = Buffer.alloc(2);
-const buf3 = Buffer.allocUnsafe(2);
-buf2[0] = 72;
-buf2[1] = 105;
-buf2.copy(buf3);
-console.log(buf2.toString());
-console.log(buf3.toString());
+const writeStream = fs.createWriteStream('./file3.txt');
+writeStream.on('finish', () => {
+    console.log('Finish!');
+})
 
-const newBuf = Buffer.concat([buf, buf2, buf3]);
-console.log(newBuf.toString());
+writeStream.write('Hello!');
+writeStream.write('World!');
+writeStream.end();
