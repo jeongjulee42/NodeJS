14401e0763267aebf74aa82857c38e79f43d429e
jeongjulee42 <ljj3342@naver.com>
2022-11-29 13:19:08 +0900

    Feat: add ejs

--- node_server_exer/simple_server/app.js
+++ node_server_exer/simple_server/app.js
@@ -1,12 +1,15 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+
+const name = 'JeongJu'
+const courses = [{name:'HTML'}, {name:'CSS'}, {name:'JS'}];
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    res.setHeader('Content-Type', 'text/html');
     if (url === '/'){
-        res.setHeader('Content-Type', 'text/html');
-        const read = fs.createReadStream('./template/index.html');
-        read.pipe(res);
+        ejs.renderFile('./template/index.ejs', {name,courses},).then(data => res.end(data))
         
     } else if (url === '/hello'){
         res.setHeader('Content-Type', 'text/html');
