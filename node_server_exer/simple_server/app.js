e29425b1aa303f34713760467b5d251a33e9126c
jeongjulee42 <ljj3342@naver.com>
2022-11-29 13:31:12 +0900

    Feat: use JSON

--- node_server_exer/simple_server/app.js
+++ node_server_exer/simple_server/app.js
@@ -1,23 +1,20 @@
 const http = require('http');
-const fs = require('fs');
-const ejs = require('ejs');
 
-const name = 'JeongJu'
 const courses = [{name:'HTML'}, {name:'CSS'}, {name:'JS'}];
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-    res.setHeader('Content-Type', 'text/html');
-    if (url === '/'){
-        ejs.renderFile('./template/index.ejs', {name,courses},).then(data => res.end(data))
-        
-    } else if (url === '/hello'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<a href="#">Click me!</a>')
-    } else {
-        res.write('Wrong');
+    const method = req.method;
+    if(url === '/courses');{
+        if(method === 'GET'){
+            res.writeHead(200, {'Content-Type':'application/json'});
+            res.end(JSON.stringify(courses));
+        }
+        else if (method === 'POST'){
+            const body = [];
+            
+        }
     }
-    
 })
 
 server.listen(8080);
