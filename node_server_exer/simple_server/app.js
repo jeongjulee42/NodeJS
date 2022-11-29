68988d6377b45c7e55fe237598f337384e1103f8
jeongjulee42 <ljj3342@naver.com>
2022-11-29 12:59:47 +0900

    Feat: add html

--- node_server_exer/simple_server/app.js
+++ node_server_exer/simple_server/app.js
@@ -1,15 +1,20 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
     if (url === '/'){
-        res.write('Welcome');
+        res.setHeader('Content-Type', 'text/html');
+        const read = fs.createReadStream('./template/index.html');
+        read.pipe(res);
+        
     } else if (url === '/hello'){
-        res.write('Hello');
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<a href="#">Click me!</a>')
     } else {
         res.write('Wrong');
     }
-    res.end();
+    
 })
 
 server.listen(8080);
