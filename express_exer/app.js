7457001082c6c27861ae78b0ab29e294be731a3c
jeongjulee42 <ljj3342@naver.com>
2022-11-30 13:31:14 +0900

    Feat: add error for test

--- express_exer/app.js
+++ express_exer/app.js
@@ -3,11 +3,15 @@ import express from 'express';
 console.clear();
 console.log('----------------------');
 const app = express();
+app.use(express.json())
 app.get('/',(req, res, next)=>{
     if(true){
         return res.send('EH')
     }
     res.send('gG')
+})
+app.post('/', (req,res,next)=>{
+    console.log(req.body)
 })
 
 app.listen(8080);
