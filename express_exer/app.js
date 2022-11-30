5b6c7c375c5c871ffe6783a9c07ce9ebc41c735d
jeongjulee42 <ljj3342@naver.com>
2022-11-30 12:26:13 +0900

    Feat: add root

--- express_exer/app.js
+++ express_exer/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
+console.clear();
+console.log('----------------------');
 const app = express();
-
-app.get('/', (req,res,next) => {
-    res.send(req.url)
-    next;
-})
-app.get('/', (req,res,next) => {
-    res.send('Hello')
+app.get('/',(req, res, next)=>{
+    if(true){
+        return res.send('EH')
+    }
+    res.send('gG')
 })
 
 app.listen(8080);
