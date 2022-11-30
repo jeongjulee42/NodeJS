74a41c22fd9240829d121fc7ea2e94dfee32d8d6
jeongjulee42 <ljj3342@naver.com>
2022-11-30 11:49:27 +0900

    Feat: add script

--- express_exer/app.js
+++ express_exer/app.js
@@ -1,5 +1,13 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
+
+app.get('/', (req,res,next) => {
+    res.send(req.url)
+    next;
+})
+app.get('/', (req,res,next) => {
+    res.send('Hello')
+})
 
 app.listen(8080);
