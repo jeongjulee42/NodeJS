99fc0cd61f772b820d29e8c5f86038e71a915e36
jeongjulee42 <ljj3342@naver.com>
2022-12-05 13:04:55 +0900

    Feat: router

---
+++ twitter_clone/server/app.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import {} from 'express-async-errors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import {} from 'cors';
+import tweetsRouter from './router/tweets.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan('tiny'));
+
+app.use('/tweets', tweetsRouter);
+
+app.use((req, res, next) => {
+    res.sendStatus(404);
+});
+
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.sendStatus(500);
+})
+
+
+app.listen(8080);
