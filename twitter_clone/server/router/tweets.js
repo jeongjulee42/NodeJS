bdf3bfd4682a2de9f275f9f3b66333be59a09209
jeongjulee42 <ljj3342@naver.com>
2022-12-05 13:15:50 +0900

    Feat: add put, delete, post

--- twitter_clone/server/router/tweets.js
+++ twitter_clone/server/router/tweets.js
@@ -27,3 +27,46 @@ router.get('/', (req, res, next) => {
     const data = username ? tweets.filter((tweet) => tweet.username === username) : tweets;
     res.status(200).json(data);
 });
+
+router.get('/', (req, res, next) => {
+    const id = req.params.id;
+    const tweet = tweets.find((tweet) => tweet.id === id);
+    if (tweet) {
+        res.status(200).json(tweet);
+    } else {
+        res.status(404).json({message: `Tweet id(${id}) not found`});
+    }
+});
+
+router.post('/', (req, res, next) => {
+    const {text, name, username} = req.body;
+    const tweet = {
+        id: Date.now().toString(),
+        text,
+        createdAt: new Date(),
+        name,
+        username
+    };
+    tweets = [tweet, ...tweets];
+    res.status(201).json(tweet);
+});
+
+router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    const text = req.body.text;
+    const tweet = tweets.find((tweet) => tweet.id === id);
+    if (tweet) {
+        tweet.text = text;
+        res.status(200).json(tweet);
+    } else {
+        res.status(404).json({message: `Tweet id(${id}) not found`});
+    }
+});
+
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    tweets = tweets.filter((tweet) => tweet.id !== id);
+    res.sendStatus(204);
+});
+
+export default router;
