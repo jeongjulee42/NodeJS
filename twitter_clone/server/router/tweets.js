b5fe7a0a40d1a3723fb2033ea91cadfa6795ef28
jeongjulee42 <ljj3342@naver.com>
2022-12-06 12:50:42 +0900

    Feat: add validator

--- twitter_clone/server/router/tweets.js
+++ twitter_clone/server/router/tweets.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import 'express-async-errors';
+import { validationResult } from 'express-validator';
 import * as tweetController from '../controller/tweet.js';
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if (errors.isEmpty()){
+        return next();
+    }
+    return res.status(400).json({message: errors.array()[0].msg})
+}
 
 
 const router = express.Router();
@@ -9,7 +18,7 @@ router.get('/', tweetController.getTweets);
 
 router.get('/:id', tweetController.getTweet);
 
-router.post('/', tweetController.createTweet);
+router.post('/', [body('text').trim().isLength({min: 3}).withMessage('text should be at least 3 characters'),], tweetController.createTweet);
 
 router.put('/:id', tweetController.updateTweet);
 
