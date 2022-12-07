b722a605a07b4f837eef6043003125621c8fe981
jeongjulee42 <ljj3342@naver.com>
2022-12-07 14:30:14 +0900

    Feat: add socket in server

--- exer_tweet/server/app.js
+++ exer_tweet/server/app.js
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
 import { config } from './config.js';
-import { Server } from 'socket.io';
+import { initSocket } from './connection/socket.js';
 
 
 const app = express();
@@ -28,14 +28,4 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 const server = app.listen(config.host.port);
-const socketIO = new Server(server, {
-  cors: {
-    origin: '*',
-  }
-});
-
-socketIO.on('connection', (socket) => {
-  console.log('Client is here!');
-  socketIO.emit('dwitter', 'Hello!')
-  socketIO.emit('dwitter', 'Hello!')
-})
+initSocket(server);
