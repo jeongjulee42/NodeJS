bf8a3ce33d603fa99041d9616dcaeab71a58b361
jeongjulee42 <ljj3342@naver.com>
2022-12-09 12:55:39 +0900

    Feat User model

--- twitter_clone/server/data/auth.js
+++ twitter_clone/server/data/auth.js
@@ -31,18 +31,13 @@ const User = sequelize.define('user',{
 )
 
 export async function findByUsername(username) {
-  return db.execute('SELECT * FROM users WHERE username=?', [username])
-    .then(result => {return result[0][0]})
+  return User.findOne({where:{username}});
 }
 
 export async function findById(id) {
-  return db.execute('SELECT * FROM users WHERE id=?', [id])
-  .then(result => {return result[0][0]})
+  return User.findByPk(id);
 }
 
 export async function createUser(user) {
-  const {username, password, name, email, url} = user;
-  return db.execute('INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)', [
-    username, password, name, email, url
-  ]).then(result => result[0].insertId);
+  return User.create(user).then(data=>{return data;});
 }
