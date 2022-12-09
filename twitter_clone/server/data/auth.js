f89618be90515661e1f4b829c48ae3000a75f4ba
jeongjulee42 <ljj3342@naver.com>
2022-12-09 12:23:33 +0900

    Feat: add User model

--- twitter_clone/server/data/auth.js
+++ twitter_clone/server/data/auth.js
@@ -1,4 +1,34 @@
-import {db} from '../db/database.js';
+import SQ from 'sequelize';
+import {db, sequelize} from '../db/database.js';
+const DataTypes = SQ.DataTypes;
+
+const User = sequelize.define('user',{
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(128),
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(128),
+      allowNull: false
+    },
+    url: DataTypes.TEXT,
+  },
+  {timestamps: false}
+)
 
 export async function findByUsername(username) {
   return db.execute('SELECT * FROM users WHERE username=?', [username])
