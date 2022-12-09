c145a9434d817cca6f0434655e3ea526804b2ca6
jeongjulee42 <ljj3342@naver.com>
2022-12-09 15:24:30 +0900

    Feat: add settings

---
+++ node/twitter_clone/server/router/tweets 2.js
@@ -0,0 +1,28 @@
+import express from "express";
+import "express-async-errors";
+import { body } from "express-validator";
+import * as tweetController from "../controller/tweet.js";
+import { validate } from "../middleware/validator.js";
+import { isAuth } from '../middleware/auth.js';
+
+const router = express.Router();
+
+const validateTweet = [
+  body("text")
+    .trim()
+    .isLength({ min: 3 })
+    .withMessage("text should be at least 3 characters"),
+  validate,
+];
+
+router.get("/", isAuth, tweetController.getTweets);
+
+router.get("/:id", isAuth, tweetController.getTweet);
+
+router.post("/", validateTweet, isAuth, tweetController.updateTweet);
+
+router.put("/:id", validateTweet, isAuth, tweetController.createTweet);
+
+router.delete("/:id", isAuth, tweetController.deleteTweet);
+
+export default router;
