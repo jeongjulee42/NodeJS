d6fd2dfcbba3dcc44aaf80f65b59b441bb8ada83
jeongjulee42 <ljj3342@naver.com>
2022-12-09 13:33:38 +0900

    Feat: close logging

--- twitter_clone/server/db/database.js
+++ twitter_clone/server/db/database.js
@@ -6,13 +6,6 @@ const {host, user, database, password} = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
     host,
     dialect: 'mysql',
+    logging: false,
 })
 
-const pool = mysql.createPool({
-    host,
-    user,
-    database,
-    password,
-});
-
-export const db = pool.promise();
