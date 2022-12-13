2999f7ff5a388f1a825e49d92c1507f8f7c8006b
jeongjulee42 <ljj3342@naver.com>
2022-12-13 15:12:29 +0900

    Feat: tweet.js

--- twitter_mongo/server/data/auth.js
+++ twitter_mongo/server/data/auth.js
@@ -22,5 +22,5 @@ export async function createUser(user) {
 }
 
 function mapOptionalUser(user){
-  return user ? { ...user, id: user._id } : user;
+  return user ? { ...user, id: user._id.toString() } : user;
 }
