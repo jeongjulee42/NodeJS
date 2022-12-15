332333db60efd2de2a5ce4a8721422335436ae58
jeongjulee42 <ljj3342@naver.com>
2022-12-15 13:12:42 +0900

    Feat: add settings for delpoy

--- twitter_clone/server/config.js
+++ twitter_clone/server/config.js
@@ -17,8 +17,10 @@ export const config = {
   bcrypt: {
     saltRounds: parseInt(required('BCRYPT_SALT_ROUNDS', 12)),
   },
-  host: {
-    port: parseInt(required('HOST_PORT', 8080)),
+  
+  port: parseInt(required('PORT', 8080)),
+  cors: {
+    allowedOrigin: required('CORS_ALLOW_ORIGIN'),
   },
   db: {
     host: required('DB_HOST'),
